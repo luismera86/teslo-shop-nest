1d08af84e0cc0488ee89277134947e8fb5f888bc
Luis Mera <luismera86@gmail.com>
2023-07-23 15:20:13 -0300

    postgres cofingurado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'test',
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true, // Cuando se realiza un cambio en las entidades, se sincroniza con la base de datos, no se utiliza en producción
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
